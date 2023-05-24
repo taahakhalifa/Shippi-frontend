3c419a40ab127f3038d8b9778a2091a923be17dc
Taaha Khalifa <taahakhalifa@outlook.com>
2023-05-24 17:09:30 +0100

    define package screen WITHOUT functionality created and renders perfectly onto screen

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./components/HomeScreen";
+import DefinePackage from "./components/DefinePackage";
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Header />
-            <HomeScreen />
-            <Footer />
+            <NavigationContainer>
+                <Header />
+                <Stack.Navigator>
+                    {/* <Stack.Screen
+                        name="Home"
+                        component={HomeScreen}
+                        options={{ headerShown: false }}
+                    /> */}
+                    <Stack.Screen
+                        name="Shipping Label"
+                        component={DefinePackage}
+                        options={{ headerShown: false }}
+                    />
+                </Stack.Navigator>
+                <Footer />
+            </NavigationContainer>
         </View>
     );
 }
---
+++ components/DefinePackage.js
@@ -0,0 +1,149 @@
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+import React from "react";
+
+export default function DefinePackage() {
+    return (
+        <SafeAreaView style={styles.screenContent}>
+            <Text style={styles.mainText}>Your updating shipping label...</Text>
+            <View style={styles.inputRow}>
+                <View
+                    style={[styles.inputRowText, styles.inputHalf, styles.icon]}
+                >
+                    <View style={styles.icon}></View>
+                    <View style={styles.icon}></View>
+                    <View style={styles.icon}></View>
+                </View>
+                <View style={styles.inputRowText}></View>
+            </View>
+            <Text style={styles.mainText}>Define your package...</Text>
+            <Text style={styles.innerText}>Weight</Text>
+            <View style={styles.kgAtEnd}>
+                <TextInput
+                    placeholder="e.g. 100"
+                    style={styles.inputWeightText}
+                ></TextInput>
+                <Text style={styles.kgText}>Kg</Text>
+            </View>
+            <Text style={styles.innerText}>Preferred Shipping Date</Text>
+            <View style={styles.datePickerContainer}></View>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.backButton}>
+                    <AntDesign name="arrowleft" size={15} color="white" />
+                    <Text style={styles.backButtonText}>Back</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.nextButton}>
+                    <Text style={styles.nextButtonText}>Next</Text>
+                    <AntDesign name="arrowright" size={15} color="white" />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    screenContent: {
+        paddingHorizontal: 25,
+        flex: 1,
+        backgroundColor: "#FAEBD7",
+    },
+    mainText: {
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    inputRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    innerText: {
+        fontSize: 15,
+        fontWeight: "bold",
+        marginTop: 15,
+        marginBottom: 10,
+    },
+    inputRowText: {
+        marginVertical: 20,
+        backgroundColor: "#E5D9CA",
+        borderRadius: 5,
+        paddingHorizontal: 20,
+        paddingVertical: 30,
+        marginBottom: 15,
+        flex: 1,
+    },
+    inputHalf: {
+        marginRight: 10,
+    },
+    inputWeightText: {
+        backgroundColor: "white",
+        borderRadius: 5,
+        padding: 12,
+        marginBottom: 15,
+        width: 300,
+    },
+    inputDateText: {
+        backgroundColor: "white",
+        borderRadius: 5,
+        padding: 12,
+        marginBottom: 15,
+    },
+    kgAtEnd: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    kgText: {
+        marginBottom: 12,
+        marginRight: 10,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    nextButton: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 100,
+        backgroundColor: "#8C7A64",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 15,
+    },
+    backButton: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 100,
+        backgroundColor: "#8C7A64",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 15,
+    },
+    nextButtonText: {
+        color: "white",
+        marginRight: 10,
+    },
+    backButtonText: {
+        color: "white",
+        marginLeft: 10,
+    },
+    datePickerContainer: {
+        backgroundColor: "white",
+        borderRadius: 5,
+        marginBottom: 15,
+        padding: 20,
+        width: "100%",
+        alignItems: "flex-start",
+    },
+});
